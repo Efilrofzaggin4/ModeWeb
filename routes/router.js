2163848942f110841a1ab0880f88c5062f6ec89e
Efilrofzaggin4 <maxime.kiniffo@3wa.io>
2023-10-04 13:06:21 +0200

    feat:admin home controller,admin home view

--- routes/router.js
+++ routes/router.js
@@ -2,15 +2,18 @@ import express from "express";
 
 const router = express.Router();
 
-                    //APPEL DES ROUTERS
+                    //APPEL DES ROUTERS VISITEUR
 import HomeController from "../controllers/HomeController.js"
 
 
 
+                    //APPEL DES ROUTERS ADMIN
 
+import AdminController from "../controllers/AdminController.js"
 
-//IMPORTATION DES ROUTES
+//IMPORTATION DES ROUTES Visiteur
 router.get('/', HomeController);
 
-
+//Importation des routes admin
+router.get('/admin', AdminController);
 export default router;
